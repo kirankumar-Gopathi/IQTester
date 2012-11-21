92f32639eb2c324f6b001690cc889e9402906c10
Jeevadeepan <jeevacse89@gmail.com>
2012-11-21 21:16:44 +0530

    Added Timer functionality

--- controller/quizController.js
+++ controller/quizController.js
@@ -9,24 +9,47 @@ App.QuizController = Ember.ArrayProxy.create({
 });
 
 (function () {
-  var json = {"title":"IQ Tester","questions":[{"question":"Two ducks and two dogs have a total of fourteen legs.","answers":["true","false"],"weight":2,"type":"radio"},{"question":"Which number should come next in the series? 1 - 1 - 2 - 3 - 5 - 8 - 13","answers":[8,13,21,26,31],"weight":2,"type":"radio"},{"question":"Which one of the five is least like the other four?","answers":["dog","mouse","lion","snake","elephant"],"weight":4,"type":"radio"},{"question":"Mary, who is sixteen years old, is four times as old as her brother. How old will Mary be when she is twice as old as her brother?","answers":[20,24,25,26,28],"weight":4,"type":"radio"},{"question":"Which one of the numbers does not belong in the following series? 2 - 3 - 6 - 7 - 8 - 14 - 15 - 30","answers":[3,7,8,15,30],"weight":4,"type":"radio"},{"question":"Which one of the five choices makes the best comparison? Finger is to Hand as Leaf is to:","answers":["twig","tree","branch","blossom","branch"],"weight":5,"type":"radio"},{"question":"If you rearrange the letters ‘CIFAIPC’ you would have the name of a(n):","answers":["city","animal","ocean","river","country"],"weight":2,"type":"radio"},{"question":"Choose the number that is 1/4 of 1/2 of 1/5 of 200:","answers":[2,5,10,25,50],"weight":4,"type":"radio"},{"question":"John needs 13 bottles of water from the store. John can only carry 3 at a time. What's the minimum number of trips John needs to make to the store?","answers":[2,4,4.5,5,6],"weight":6,"type":"radio"},{"question":"Who is the famous personality?","imageURL":"http://ia.media-imdb.com/images/M/MV5BMTIwMzAwMzg1MV5BMl5BanBnXkFtZTYwMjc4ODQ2._V1._SX214_CR0,0,214,314_.jpg","weight":8,"type":"fillin"}],"time":120,"randomized":true};
+  
+  var json = {"title":"IQ Tester","questions":[{"question":"Two ducks and two dogs have a total of fourteen legs.","answers":["true","false"],"weight":2,"type":"radio"},{"question":"Which number should come next in the series? 1 - 1 - 2 - 3 - 5 - 8 - 13","answers":[8,13,21,26,31],"weight":2,"type":"radio"},{"question":"Which one of the five is least like the other four?","answers":["dog","mouse","lion","snake","elephant"],"weight":4,"type":"radio"},{"question":"Mary, who is sixteen years old, is four times as old as her brother. How old will Mary be when she is twice as old as her brother?","answers":[20,24,25,26,28],"weight":4,"type":"radio"},{"question":"Which one of the numbers does not belong in the following series? 2 - 3 - 6 - 7 - 8 - 14 - 15 - 30","answers":[3,7,8,15,30],"weight":4,"type":"radio"},{"question":"Which one of the five choices makes the best comparison? Finger is to Hand as Leaf is to:","answers":["twig","tree","branch","blossom","branch"],"weight":5,"type":"radio"},{"question":"If you rearrange the letters ‘CIFAIPC’ you would have the name of a(n):","answers":["city","animal","ocean","river","country"],"weight":2,"type":"radio"},{"question":"Choose the number that is 1/4 of 1/2 of 1/5 of 200:","answers":[2,5,10,25,50],"weight":4,"type":"radio"},{"question":"John needs 13 bottles of water from the store. John can only carry 3 at a time. What's the minimum number of trips John needs to make to the store?","answers":[2,4,4.5,5,6],"weight":6,"type":"radio"},{"question":"Who is the famous personality?","imageURL":"http://ia.media-imdb.com/images/M/MV5BMTIwMzAwMzg1MV5BMl5BanBnXkFtZTYwMjc4ODQ2._V1._SX214_CR0,0,214,314_.jpg","weight":8,"type":"fillin"}],"time": 120,"randomized":true};
   App.QuizController.set('randomized',json.randomized);
   App.QuizController.set('timer',json.time);
-	var questions = json["questions"];
+  var questions = json["questions"];
   var questionModelArr = [];
+   
+   $("button#startTestBtn").one("click", function () {
+      $(this).hide();
+	  initializeTimer(); 
+	  for (var i = 0, len = questions.length; i < len; i++) {
+			var question = App.Question.create();
+		question.set('question',questions[i].question);
+		question.set('options',questions[i].answers);
+		question.set('type',questions[i].type);
+		question.set('weight',questions[i].weight);
+		question.set('imageURL',questions[i].imageURL);
+		questionModelArr.push(question);
+		}
 
-  for (var i = 0, len = questions.length; i < len; i++) {
- 		var question = App.Question.create();
-    question.set('question',questions[i].question);
-    question.set('options',questions[i].answers);
-    question.set('type',questions[i].type);
-    question.set('weight',questions[i].weight);
-    question.set('imageURL',questions[i].imageURL);
-    questionModelArr.push(question);
- 	}
-
-  if(questionModelArr.length > 1){
- 			var consolidatedRecords = [];
- 			App.QuizController.set('[]', questionModelArr);
- 	}
+	  if(questionModelArr.length > 1){
+				var consolidatedRecords = [];
+				App.QuizController.set('[]', questionModelArr);
+		}
+	});
+	
+   // Chech for timer parameter and initialize timer.
+   var initializeTimer = function () {
+     if(App.QuizController.timer){
+	   var quizTimer =  App.QuizController.timer;
+	   $("div#timer").show().children("span").html(quizTimer);
+       var updateTimer = window.setInterval(function () {
+	      if(quizTimer > 0){
+ 		       $("div#timer span").html(--quizTimer);
+            }  
+		  else{
+               clearInterval(updateTimer);
+			    $("div#timer").html("Time Up!");
+			}
+	  }, 1000);
+     }
+	}
+	
 })();
