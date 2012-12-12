4c038aba8545805b6ff65e07ca7a34299534d51b
viswanathm <viswanathm@viswanath-m.(none)>
2012-12-12 20:40:16 +0530

    updated test cases and moved file into different folder

--- js/myTests.js
+++ js/myTests.js
@@ -5,3 +5,23 @@ test('shuffleContent()', function() {
     var y = App.QuizController.get('content')[0];
     notDeepEqual(x,y, 'content shuffled');
 })
+
+test('pass()', function() {
+    App.QuizController.set('currentQuestionIndex',1);
+    App.QuizController.pass();
+    var y = App.QuizController.get('currentQuestionIndex');
+    equal(y,2, 'passed to next question, updated currentQuestionIndex');
+})
+
+test('next()', function() {
+    var question = Ember.Object.create({isAnswerCorrect:true,weight:4});
+    App.QuizController.set('content',[question]);
+    App.QuizController.set('currentQuestionIndex',0);
+    App.QuizController.set('correctAnswersCount',0);
+    App.QuizController.set('score',0);
+    App.QuizController.next();
+    var correctAnswersCount = App.QuizController.get('correctAnswersCount');
+    equal(correctAnswersCount,1, 'incremented correctAnswersCount by one');
+    var score = App.QuizController.get('score');
+    equal(score,4, 'updated score with weight of currentQuestion');
+})
